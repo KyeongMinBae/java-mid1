35de9a91a480234467a5ea9c4b9879ef41bacbe5
KyeongMinBae <reaaaaaaal@gmail.com>
2025-11-29 19:35:35 +0900

    패키지 위치 변경

--- src/lang/immutable/address/RefMain1_1.java
+++ src/lang/immutable/address/RefMain1_1.java
@@ -1,16 +1,15 @@
-package lang.object.immutable.address;
+package lang.immutable.address;
 
-public class RefMain2 {
+public class RefMain1_1 {
 
     public static void main(String[] args) {
         //참조형 변수는 하나의 인스턴스를 공유할 수 있다.
-        ImmutableAddress a = new ImmutableAddress("서울");
-        ImmutableAddress b = a; //참조값 대입을 막을 수 있는 방법이 없다.
+        Address a = new Address("서울");
+        Address b = a;
         System.out.println("a = " + a);
         System.out.println("b = " + b);
 
-        //b.setValue("부산"); //b읙 값을 부산으로 변경해야함
-        b = new ImmutableAddress("부산");
+        b.setValue("부산"); //b읙 값을 부산으로 변경해야함
         System.out.println("부산 -> b");
         System.out.println("a = " + a);
         System.out.println("b = " + b);
