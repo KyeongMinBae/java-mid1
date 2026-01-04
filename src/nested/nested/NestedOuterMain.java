d6bff1dc30d4d6f69043007805c44c25d5418954
KyeongMinBae <reaaaaaaal@gmail.com>
2026-01-04 15:05:04 +0900

    내부 클래스의 활용

--- src/nested/nested/NestedOuterMain.java
+++ src/nested/nested/NestedOuterMain.java
@@ -4,6 +4,7 @@ public class NestedOuterMain {
 
     public static void main(String[] args) {
         NestedOuter outer = new NestedOuter();
+
         NestedOuter.Nested nested = new NestedOuter.Nested();
         nested.print();
 
