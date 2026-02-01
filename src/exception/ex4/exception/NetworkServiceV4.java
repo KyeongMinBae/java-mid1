6a0cd73dc9083d2761281a3c36ea45cd8fb2c835
KyeongMinBae <reaaaaaaal@gmail.com>
2026-02-01 13:45:01 +0900

    실무 예외 처리 방안2 - 구현

---
+++ src/exception/ex4/exception/NetworkServiceV4.java
@@ -0,0 +1,20 @@
+package exception.ex4.exception;
+
+import exception.ex4.NetworkClientV4;
+
+public class NetworkServiceV4 {
+
+    public void sendMessage(String data) {
+        String address = "http://example.com";
+        NetworkClientV4 client = new NetworkClientV4(address);
+        client.initError(data); //추가
+
+
+        try {
+            client.connect();
+            client.send(data);
+        } finally {
+            client.disconnect();
+        }
+    }
+}
